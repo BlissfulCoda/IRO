993da3db2b622b6c59e7b2cc8b1133f22783b7a6
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-12-29 06:06:41 +0000

    created globalDevTools

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
 
 // Set Environment
 process.env.NODE_ENV = 'development'
@@ -26,6 +26,9 @@ app.on('ready', () => {
   const mainMenu = Menu.buildFromTemplate(menu)
   Menu.setApplicationMenu(mainMenu)
 
+  globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
+  globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
+
   mainWindow.on('closed', () => mainWindow = null)
 });
 
@@ -38,6 +41,7 @@ const menu = [
     submenu: [
       {
         label: 'Quit',
+        accelerator: 'CmdOrCtrl+W',
         click: () => app.quit()      
       }
     ]
