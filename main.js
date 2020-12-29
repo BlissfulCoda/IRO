1bfa214247e8c0158379a52dd8511c5975e9b1c7
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-12-29 17:25:33 +0000

    creaeted shortcuts and submenus with roles

--- main.js
+++ main.js
@@ -1,63 +1,67 @@
 const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
 
 // Set Environment
-process.env.NODE_ENV = 'development'
+process.env.NODE_ENV = 'development';
 
-const isDev = process.env.NODE_ENV !== 'production' ? true : false
-const isMac = process.platform == 'darwin' ? true : false
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform == 'darwin' ? true : false;
 
 let mainWindow;
 
 function createMainWindow() {
-    mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 500,
     title: 'Image Resize & Optimiser',
     height: 600,
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-    resizable: isDev,
+    resizable: isDev
   });
 
   mainWindow.loadFile('./app/index.html');
-};
-
+}
 
 app.on('ready', () => {
   createMainWindow();
-  const mainMenu = Menu.buildFromTemplate(menu)
-  Menu.setApplicationMenu(mainMenu)
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
 
-  globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
-  globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
+  globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload());
+  globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () =>
+    mainWindow.toggleDevTools()
+  );
 
-  mainWindow.on('closed', () => mainWindow = null)
+  mainWindow.on('closed', () => (mainWindow = null));
 });
 
 const menu = [
-  ...(isMac ? [
-    { role: 'appMenu'}
-  ] : []),
+  ...(isMac ? [{ role: 'appMenu' }] : []),
   {
-    label: 'File',
-    submenu: [
-      {
-        label: 'Quit',
-        accelerator: 'CmdOrCtrl+W',
-        click: () => app.quit()      
-      }
-    ]
-  }
+    role: 'fileMenu'
+  },
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' }
+          ]
+        }
+      ]
+    : [])
 ];
-
 
 // Keep the app on to explicitly close it
 app.on('window-all-closed', () => {
   if (!isMac) {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createMainWindow()
+    createMainWindow();
   }
-})
+});
