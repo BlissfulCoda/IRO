69980a34ad2fa634a9aeba2fc7798fe85368f98f
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-12-29 23:47:07 +0000

    packaged the app for  production

--- app/js/render.js
+++ app/js/render.js
@@ -2,30 +2,29 @@ const path = require('path');
 const os = require('os');
 const { ipcRenderer } = require('electron');
 
-// Form EventListner
-const form = document.querySelector('#image-form');
-const slider = document.querySelector('#slider');
-const img = document.querySelector('#img');
-const log = require('electron-log');
+const form = document.getElementById('image-form');
+const slider = document.getElementById('slider');
+const img = document.getElementById('img');
 
-document.querySelector('#output-path').innerText = path.join(
+document.getElementById('output-path').innerText = path.join(
   os.homedir(),
-  'iro'
+  'pictune'
 );
 
-// OnSubmit
+//   Onsubmit
 form.addEventListener('submit', e => {
   e.preventDefault();
 
   const imgPath = img.files[0].path;
   const quality = slider.value;
+
   ipcRenderer.send('image:minimize', {
     imgPath,
     quality
   });
 });
 
-// On Done
+//   On done
 ipcRenderer.on('image:done', () => {
   M.toast({
     html: `Image resized to ${slider.value}% quality`
--- main.js
+++ main.js
@@ -1,87 +1,62 @@
-const path = require('path');
-const os = require('os');
-const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
-const imagemin = require('imagemin');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const imageminPngquant = require('imagemin-pngquant');
-const slash = require('slash');
+const path = require('path')
+const os = require('os')
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron')
+const imagemin = require('imagemin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
+const slash = require('slash')
+const log = require('electron-log')
 
-// Set Environment
-process.env.NODE_ENV = 'production';
+// Set env
+process.env.NODE_ENV = 'production'
 
-const isDev = process.env.NODE_ENV !== 'production' ? true : false;
-const isMac = process.platform == 'darwin' ? true : false;
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+const isMac = process.platform === 'darwin' ? true : false
 
-let mainWindow;
-let aboutWindow;
+let mainWindow
+let aboutWindow
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
-    width: isDev ? 800 : 450,
-    title: 'Image Resize & Optimiser',
+    title: 'Pictune',
+    width: isDev ? 800 : 500,
     height: 600,
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-    resizable: isDev,
+    resizable: isDev ? true : false,
     backgroundColor: 'white',
     webPreferences: {
-      nodeIntegration: true
-    }
-  });
+      nodeIntegration: true,
+    },
+  })
 
   if (isDev) {
-    mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools()
   }
-  mainWindow.loadFile('./app/index.html');
+
+  mainWindow.loadFile('./app/index.html')
 }
 
-// image:minimize
-ipcMain.on('image:minimize', (e, Options) => {
-  Options.dest = path.join(os.homedir(), 'iro');
-  shrinkImage(Options);
-});
-
-async function shrinkImage({ imgPath, quality, dest }) {
-  try {
-    const pngQuality = quality / 100;
-
-    const files = await imagemin([slash(imgPath)], {
-      destination: dest,
-      plugins: [
-        imageminMozjpeg({ quality }),
-        imageminPngquant({
-          quality: [pngQuality, pngQuality]
-        })
-      ]
-    });
-    log.info(filed);
-    shell.openPath(dest);
-
-    mainWindow.webContents.send('image:done');
-  } catch (err) {
-    log.error(err);
-  }
-}
-
-// About Window
 function createAboutWindow() {
   aboutWindow = new BrowserWindow({
-    title: 'About Image Resize & Optimiser',
+    title: 'About Pictune',
     width: 300,
     height: 300,
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
     resizable: false,
-    backgroundColor: 'white'
-  });
+    backgroundColor: 'white',
+  })
 
-  aboutWindow.loadFile('./app/about.html');
+  aboutWindow.loadFile('./app/about.html')
 }
 
 app.on('ready', () => {
-  createMainWindow();
-  const mainMenu = Menu.buildFromTemplate(menu);
-  Menu.setApplicationMenu(mainMenu);
-  mainWindow.on('closed', () => (mainWindow = null));
-});
+  createMainWindow()
+
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
+
+  mainWindow.on('ready', () => (mainWindow = null))
+})
 
 const menu = [
   ...(isMac
@@ -91,15 +66,28 @@ const menu = [
           submenu: [
             {
               label: 'About',
-              click: createAboutWindow
-            }
-          ]
-        }
+              click: createAboutWindow,
+            },
+          ],
+        },
       ]
     : []),
   {
-    role: 'fileMenu'
+    role: 'fileMenu',
   },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
   ...(isDev
     ? [
         {
@@ -108,22 +96,53 @@ const menu = [
             { role: 'reload' },
             { role: 'forcereload' },
             { type: 'separator' },
-            { role: 'toggledevtools' }
-          ]
-        }
+            { role: 'toggledevtools' },
+          ],
+        },
       ]
-    : [])
-];
+    : []),
+]
 
-// Keep the app on to explicitly close it
+ipcMain.on('image:minimize', (e, options) => {
+  options.dest = path.join(os.homedir(), 'imageshrink')
+  shrinkImage(options)
+})
+
+async function shrinkImage({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100
+
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({
+          quality: [pngQuality, pngQuality],
+        }),
+      ],
+    })
+
+    log.info(files)
+
+//     Changed from shell.openItem() for v9
+    shell.openPath(dest)
+
+    mainWindow.webContents.send('image:done')
+  } catch (err) {
+    log.error(err)
+  }
+}
+
 app.on('window-all-closed', () => {
   if (!isMac) {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createMainWindow();
+    createMainWindow()
   }
-});
+})
+
+app.allowRendererProcessReuse = true
